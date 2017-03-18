8295f912e06b59ca19dd46774d818870702ec706
Rhys Brame <rhysbrame@gmail.com>
2017-03-18 13:03:52 +0000

    added complete song class

---
+++ songs.rb
@@ -0,0 +1,11 @@
+class Song
+
+  attr_reader :name
+
+  def initialize(song_name)
+    @name = song_name
+  end
+
+
+
+end
---
+++ spec/songs_spec.rb
@@ -0,0 +1,15 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../songs')
+
+class TestSongs < MiniTest::Test
+
+  def setup
+    @song = Song.new("Sweet Child of Mine")
+  end
+
+  def test_song_has_name
+    assert_equal("Sweet Child of Mine", @song.name)
+  end
+
+end
